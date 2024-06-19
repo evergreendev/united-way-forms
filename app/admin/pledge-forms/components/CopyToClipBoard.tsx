8cec965c30af89753522f80e3be3cb76b089b2b0
Joe simpson <joe@egmrc.com>
2024-06-19 11:00:02 -0600

    Add pledge form urls

--- app/admin/pledge-forms/components/CopyToClipBoard.tsx
+++ app/admin/pledge-forms/components/CopyToClipBoard.tsx
@@ -12,7 +12,7 @@ const CopyToClipBoard = ({text}: { text:string }) => {
 
 
     return <button className={`rounded font-bold bg-blue-900 transition-all text-white p-1 m-2 ${copying ? "bg-slate-800" : "bg-blue-900"}`} onClick={() => {
-        navigator.clipboard.writeText(window.location.origin+"/"+text).then(value => {
+        navigator.clipboard.writeText(window.location.origin+text).then(value => {
             setCopying(true);
         })
     }}>{copying ? "Copied!" : "Copy form URL to clipboard"}</button>
